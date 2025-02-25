832936e3b094b32703a0204cc589225a5ca5a6cf
Prem <premkumar25122003@gmail.com>
2025-02-25 12:41:32 +0530

    Build Successful

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -1,25 +1,25 @@
-import useTrailers from '../hooks/useTrailers'
+import useTrailers from '../hooks/useTrailers';
 
-interface Props{
+interface Props {
     gameId: number;
 }
 
 const GameTrailer = ({ gameId }: Props) => {
-    const { data, error, isLoading} = useTrailers(gameId);
+    const { data, error, isLoading } = useTrailers(gameId);
 
     if (isLoading) return null;
-
     if (error) throw error;
-    
-    const first = data?.results[0];
-    return first? (
-        <video 
-        src={first.data[480]}
-        poster={first.preview}
-        controls
-        />
-      ) : null;
-  
-}
 
-export default GameTrailer
+    const trailer = data?.results?.[0];
+    if (!trailer) return null; 
+
+    return (
+        <video 
+            src={trailer?.data[480]} 
+            poster={trailer?.preview} 
+            controls
+        />
+    );
+};
+
+export default GameTrailer;
